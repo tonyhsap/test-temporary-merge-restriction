535bcdef186bc5cf653aa2b9897ee23a1e940142
Tony Heiden <95026863+tonyhsap@users.noreply.github.com>
2025-03-13 12:56:00 +0100

    Update determine_action.py

--- .github/workflows/determine_action.py
+++ .github/workflows/determine_action.py
@@ -9,14 +9,17 @@ def determine_action():
     # Get the current time in the target timezone
     local_time = datetime.now(tz)
 
-    # Desired times for action
+    # Determine the action based on the current time
     if local_time.hour == 11 and local_time.minute == 30:
-        print("ACTION=add")
+        action = "add"
     elif local_time.hour == 13 and local_time.minute == 15:
-        print("ACTION=remove")
+        action = "remove"
     else:
-        print("ACTION=none")
+        action = "none"
 
+    # Write the action to $GITHUB_ENV so it can be used in subsequent steps
+    with open('/github/env', 'a') as env_file:
+        env_file.write(f"ACTION={action}\n")
 
 if __name__ == "__main__":
     determine_action()
